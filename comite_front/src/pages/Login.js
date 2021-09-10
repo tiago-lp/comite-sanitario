8f1e3ccd57982559c2b979e8e8a2558673299186
tiago-lp <tiagolprr@gmail.com>
2021-09-09 22:16:09 -0300

    feat: template added

---
+++ comite_front/src/pages/Login.js
@@ -0,0 +1,47 @@
+// material
+import { styled } from '@material-ui/core/styles';
+import { Stack, Container, Typography } from '@material-ui/core';
+// components
+import Page from '../components/Page';
+import { LoginForm } from '../components/authentication/login';
+
+// ----------------------------------------------------------------------
+
+const RootStyle = styled(Page)(({ theme }) => ({
+  [theme.breakpoints.up('md')]: {
+    display: 'flex'
+  }
+}));
+
+
+const ContentStyle = styled('div')(({ theme }) => ({
+  maxWidth: 480,
+  margin: 'auto',
+  display: 'flex',
+  minHeight: '100vh',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  padding: theme.spacing(12, 0)
+}));
+
+// ----------------------------------------------------------------------
+
+export default function Login() {
+  return (
+    <RootStyle title="Login">
+
+      <Container maxWidth="sm">
+        <ContentStyle>
+          <Stack sx={{ mb: 5 }}>
+            <Typography variant="h4" gutterBottom>
+              Login
+            </Typography>
+            <Typography sx={{ color: 'text.secondary' }}>Entre com email e senha.</Typography>
+          </Stack>
+          <LoginForm />
+
+        </ContentStyle>
+      </Container>
+    </RootStyle>
+  );
+}
---
+++ comite_front/src/routes.js
@@ -0,0 +1,37 @@
+import { Navigate, useRoutes } from 'react-router-dom';
+// layouts
+import DashboardLayout from './layouts/dashboard';
+import LogoOnlyLayout from './layouts/LogoOnlyLayout';
+//
+import Login from './pages/Login';
+import DashboardApp from './pages/DashboardApp';
+import User from './pages/User';
+import NotFound from './pages/Page404';
+
+// ----------------------------------------------------------------------
+
+export default function Router() {
+  return useRoutes([
+    {
+      path: '/dashboard',
+      element: <DashboardLayout />,
+      children: [
+        { path: '/', element: <Navigate to="/dashboard/app" replace /> },
+        { path: 'app', element: <DashboardApp /> },
+        { path: 'user', element: <User /> },
+      ]
+    },
+    {
+      path: '/',
+      element: <LogoOnlyLayout />,
+      children: [
+        { path: 'login', element: <Login /> },
+        { path: '404', element: <NotFound /> },
+        { path: '/', element: <Navigate to="/dashboard" /> },
+        { path: '*', element: <Navigate to="/404" /> }
+      ]
+    },
+
+    { path: '*', element: <Navigate to="/404" replace /> }
+  ]);
+}
