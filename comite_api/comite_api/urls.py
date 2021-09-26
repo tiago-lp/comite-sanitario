a00e0cb727a930befb8b257a0bd2e112da9c9a80
tiago-lp <tiagolprr@gmail.com>
2021-09-26 17:40:31 -0300

    build: homolog settings

--- comite_api/comite_api/urls.py
+++ comite_api/comite_api/urls.py
@@ -5,6 +5,7 @@ from rest_framework import routers
 from comite_api.settings import DEBUG
 from core import views
 from rest_framework_simplejwt import views as jwt_views
+# from django.conf.urls import url
 
 
 router = routers.DefaultRouter()
@@ -17,6 +18,7 @@ urlpatterns = [
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include(router.urls)),
     path('api/admin/', admin.site.urls),
+    # url(r'^', views.FrontendAppView.as_view())
 ]
 
 if DEBUG:
